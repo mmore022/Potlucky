537f13c5b99fad52810f5fddbc5b6a0d3505a20a
tmoor008 <tmoor008@ucr.edu>
2017-04-08 21:07:24 -0700

    i tried

---
+++ app/src/main/java/com/lucky/pot/potlucky/Profile.java
@@ -0,0 +1,15 @@
+package com.lucky.pot.potlucky;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by Tia on 4/8/2017.
+ */
+
+public class Profile extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_layout);
+    }
+}
