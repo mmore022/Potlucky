2bf692f80cea83fc5aa739da525239ae3e43522d
tmoor008 <tmoor008@ucr.edu>
2017-04-08 22:46:08 -0700

    created profile layout

---
+++ app/src/main/java/com/lucky/pot/potlucky/PartyCreator.java
@@ -0,0 +1,8 @@
+package com.lucky.pot.potlucky;
+
+/**
+ * Created by Tia on 4/8/2017.
+ */
+
+public class PartyCreator {
+}
--- app/src/main/java/com/lucky/pot/potlucky/Profile.java
+++ app/src/main/java/com/lucky/pot/potlucky/Profile.java
@@ -2,6 +2,7 @@ package com.lucky.pot.potlucky;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by Tia on 4/8/2017.
@@ -11,5 +12,18 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_layout);
+    }
+
+    public void onSelect(View view)
+    {
+        switch(view.getId())
+        {
+            case R.id.startParty:
+                //do
+            case R.id.hostParty:
+                //do
+            case R.id.invitations:
+                //do
+        }
     }
 }
